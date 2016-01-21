a1dfe1a41533791af9a39723d0e12c1c56dfd567
jfc3 <buckslice@gmail.com>
2016-01-20 21:20:21 -0800

    renaming classes

--- Game.h
+++ Game.h
@@ -1,30 +1,66 @@
 #pragma once
 
-#include "player.h"
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <memory>
+
+#include "shader.h"
+#include "camera.h"
+#include "cityGenerator.h"
+#include "terrainGenerator.h"
+#include "glHelper.h"
+#include "physics.h"
+#include "menu.h"
+#include "game.h"
+#include "graphics.h"
 #include "input.h"
-
-class Game {
+#include "enemy.h"
+#include "mathutil.h"
+#include "entityManager.h"
+class Game
+{
 public:
-    Game(int width, int height) {
-        running = false;
+	Game(int width, int height);
+	~Game();
 
-    }
+	void mainLoop();
 
-    bool gameIsOver() {
-    }
-
-    void setRunning(bool b) {
-        running = b;
-    }
-
-    bool isRunning() {
-        return running;
-    }
-
-    void update(sf::RenderWindow& window) {
-
-    }
-
+	bool isRunning();
 private:
-    bool running;
+	sf::Vector2i center;
+	int width;
+	int height;
+	bool running;
+	sf::Music mainTrack;
+	sf::RenderWindow* window;
+	Graphics* graphics;
+	Physics* physics;
+	Input input;
+	CityGenerator* cg;
+    TerrainGenerator* tg;
+	Camera cam;
+	Player* player;
+	EntityManager* em;
+	Menu* menu;
+
+	// some utility clocks
+	sf::Clock frameTime;
+	sf::Clock animTime;
+	bool lastFocused;
+	bool gameFocused;
+	bool clickedInside;
+	bool mouseVisible;
+	bool lastMouseVisible;
+
+
 };
+
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include "gameWindow.h"
+#include "game.h"
 const int WIDTH = 1024;
 const int HEIGHT = 768;
 int main() {
-	GameWindow gameWindow(WIDTH, HEIGHT);
-	gameWindow.mainLoop();
+	Game game(WIDTH, HEIGHT);
+    game.mainLoop();
 
     return 0;
 }
